95bc461d4203912079253fd359eb1e094635cf56
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-10-12 20:29:47 +0600

    removed: login page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ["https://tailwindui.com"]
+    }
+}
 
 module.exports = nextConfig
