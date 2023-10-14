67bb4f5193e0a124190e47ec7aa4b4af99411f98
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-10-14 18:17:05 +0600

    MONGODB_URI added

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         domains: ["https://tailwindui.com", "img.freepik.com"],
     },
     env: {
-        MONGODB_URI: `mongodb+srv://saidur:xI1gm3qFbzdzsJVp@saidur.2jjypfv.mongodb.net/?retryWrites=true&w=majority`,
+        MONGODB_URI: `mongodb+srv://saidurrahman:saidur@cluster0.qf4haff.mongodb.net/?retryWrites=true&w=majority`,
         baseURL:
             process.env.NODE_ENV !== "production"
                 ? "http://localhost:3000"
