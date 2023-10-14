28a7efdf6bcab723f7d48722afa51a71d840ddab
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-10-14 17:59:10 +0600

    URI change

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("DB Connected");
     } catch (error) {
         throw new Error("Connection failed!");
