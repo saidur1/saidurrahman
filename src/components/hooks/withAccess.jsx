ccf5148ef7b7745af53b051ca92719c53902ff66
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-10-14 07:52:31 +0600

    wrapper change

--- src/components/hooks/withAccess.jsx
+++ src/components/hooks/withAccess.jsx
@@ -2,7 +2,7 @@ import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 
 const withAccess = (WrappedComponent) => {
-    return (props) => {
+    const WithAccess = (props) => {
         const router = useRouter();
 
         // Your authentication logic here (e.g., checking if the user is authenticated)
@@ -14,6 +14,16 @@ const withAccess = (WrappedComponent) => {
 
         return <WrappedComponent {...props} />;
     };
+
+    // Add a displayName to the HOC
+    WithAccess.displayName = `withAccess(${getDisplayName(WrappedComponent)})`;
+
+    return WithAccess;
 };
+
+// A utility function to get the display name of a component
+function getDisplayName(WrappedComponent) {
+    return WrappedComponent.displayName || WrappedComponent.name || "Component";
+}
 
 export default withAccess;
