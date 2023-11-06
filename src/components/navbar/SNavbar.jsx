9fd3c1c3c547747737e9e8337cccbceb0fd5bf9e
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-11-06 14:50:48 +0600

    modified: link of our story page

--- src/components/navbar/SNavbar.jsx
+++ src/components/navbar/SNavbar.jsx
@@ -17,7 +17,7 @@ const navigation = [
         icon: true,
         submenus: [
             { name: "Contact", href: "/contact", current: false },
-            { name: "Our Story", href: "/our_story", current: false },
+            { name: "Our Story", href: "/story", current: false },
             {
                 name: "Mission, Vision",
                 href: "/mission&Vision",
@@ -131,7 +131,7 @@ export default function SNavbar() {
                                             </Menu.Item>
                                             <Menu.Item>
                                                 <Link
-                                                    href="/our_story"
+                                                    href="/story"
                                                     className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium block"
                                                 >
                                                     Our Story
