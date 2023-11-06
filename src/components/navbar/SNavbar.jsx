474933c141f6b00f6270cde8090e99e3787f15d7
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-11-06 14:52:38 +0600

    modified: link of mission and vision page

--- src/components/navbar/SNavbar.jsx
+++ src/components/navbar/SNavbar.jsx
@@ -20,7 +20,7 @@ const navigation = [
             { name: "Our Story", href: "/story", current: false },
             {
                 name: "Mission, Vision",
-                href: "/mission&Vision",
+                href: "/mission",
                 current: false,
             },
         ],
@@ -139,7 +139,7 @@ export default function SNavbar() {
                                             </Menu.Item>
                                             <Menu.Item>
                                                 <Link
-                                                    href="/mission&Vision"
+                                                    href="/mission"
                                                     className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium block"
                                                 >
                                                     Mission, Vision
