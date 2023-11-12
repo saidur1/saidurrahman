d852991c4f44475937fbd6181929ac847a3bf102
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-11-12 14:54:46 +0600

    updated: dropdoen text

--- src/components/navbar/SNavbar.jsx
+++ src/components/navbar/SNavbar.jsx
@@ -124,7 +124,7 @@ export default function SNavbar() {
                                             <Menu.Item>
                                                 <Link
                                                     href="/contact"
-                                                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium block"
+                                                    className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium block"
                                                 >
                                                     Contact
                                                 </Link>
@@ -132,7 +132,7 @@ export default function SNavbar() {
                                             <Menu.Item>
                                                 <Link
                                                     href="/story"
-                                                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium block"
+                                                    className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium block"
                                                 >
                                                     Our Story
                                                 </Link>
@@ -140,7 +140,7 @@ export default function SNavbar() {
                                             <Menu.Item>
                                                 <Link
                                                     href="/mission"
-                                                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium block"
+                                                    className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium block"
                                                 >
                                                     Mission, Vision
                                                 </Link>
