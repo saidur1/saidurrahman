29f5cbf4c472f827f868ae474d3e6c506ee7f3b1
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-11-16 14:29:20 +0600

    chage route

--- src/components/JoinDiscord/Discord.js
+++ src/components/JoinDiscord/Discord.js
@@ -4,7 +4,7 @@ const Discord = () => {
     return (
         <div>
             <Link
-                href="/accountability-community/form"
+                href="/accountability-community/request"
                 className="bg-black/70 hover:bg-black/90 text-white/80 rounded-sm px-10 py-2 duration-200 "
             >
                 Join Now
