a5f4e2204bfc964faf8835f9bdc2c854d729c947
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-11-16 15:17:27 +0600

    gh

--- src/components/JoinDiscord/Discord.js
+++ src/components/JoinDiscord/Discord.js
@@ -1,14 +1,52 @@
-import Link from "next/link";
+"use client";
+import {
+    Button,
+    Dialog,
+    DialogBody,
+    DialogFooter,
+    DialogHeader,
+} from "@material-tailwind/react";
+import { useState } from "react";
 
 const Discord = () => {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(!open);
     return (
         <div>
-            <Link
-                href="/accountability-community/request"
+            <button
+                // href="/accountability-community/request"
                 className="bg-black/70 hover:bg-black/90 text-white/80 rounded-sm px-10 py-2 duration-200 "
+                onClick={handleOpen}
             >
                 Join Now
-            </Link>
+            </button>
+            <Dialog open={open} handler={handleOpen}>
+                <DialogHeader>Its a simple dialog.</DialogHeader>
+                <DialogBody>
+                    The key to more success is to have a lot of pillows. Put it
+                    this way, it took me twenty five years to get these plants,
+                    twenty five years of blood sweat and tears, and I&apos;m
+                    never giving up, I&apos;m just getting started. I&apos;m up
+                    to something. Fan luv.
+                </DialogBody>
+                <DialogFooter>
+                    <Button
+                        variant="text"
+                        color="red"
+                        onClick={handleOpen}
+                        className="mr-1"
+                    >
+                        <span>Cancel</span>
+                    </Button>
+                    <Button
+                        variant="gradient"
+                        color="green"
+                        onClick={handleOpen}
+                    >
+                        <span>Confirm</span>
+                    </Button>
+                </DialogFooter>
+            </Dialog>
         </div>
     );
 };
