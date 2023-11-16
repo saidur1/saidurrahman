8f7443f460031265580a7b14b392462751b353d4
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-11-16 09:28:33 +0600

    added: modal

--- src/components/JoinDiscord/Discord.js
+++ src/components/JoinDiscord/Discord.js
@@ -1,4 +1,11 @@
 "use client";
+import {
+    Button,
+    Dialog,
+    DialogBody,
+    DialogFooter,
+    DialogHeader,
+} from "@material-tailwind/react";
 import { useState } from "react";
 
 const Discord = () => {
@@ -14,6 +21,33 @@ const Discord = () => {
             >
                 Join Now
             </button>
+            <Dialog open={open} handler={handleOpen}>
+                <DialogHeader>Its a simple dialog.</DialogHeader>
+                <DialogBody>
+                    The key to more success is to have a lot of pillows. Put it
+                    this way, it took me twenty five years to get these plants,
+                    twenty five years of blood sweat and tears, and I&apos;m
+                    never giving up, I&apos;m just getting started. I&apos;m up
+                    to something. Fan luv.
+                </DialogBody>
+                <DialogFooter>
+                    <Button
+                        variant="text"
+                        color="red"
+                        onClick={handleOpen}
+                        className="mr-1"
+                    >
+                        <span>Cancel</span>
+                    </Button>
+                    <Button
+                        variant="gradient"
+                        color="green"
+                        onClick={handleOpen}
+                    >
+                        <span>Confirm</span>
+                    </Button>
+                </DialogFooter>
+            </Dialog>
         </div>
     );
 };
