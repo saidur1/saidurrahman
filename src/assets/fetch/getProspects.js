f051f7153502ebe348a180bd9973f05992f30575
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-11-21 14:43:36 +0600

    fds

--- src/assets/fetch/getProspects.js
+++ src/assets/fetch/getProspects.js
@@ -5,11 +5,16 @@ export async function getProspects() {
             cache: "no-store",
         });
         const data = await res.json();
-        const users = data?.map(({ _id, ...rest }) => ({
-            id: _id,
-            ...rest,
-        }));
-        return users;
+
+        if (!res.ok) {
+            console.log("can't get prospects");
+        } else {
+            const users = data?.map(({ _id, ...rest }) => ({
+                id: _id,
+                ...rest,
+            }));
+            return users;
+        }
     } catch (error) {
         console.log("FETCH in <getProspect>", error);
         return [];
