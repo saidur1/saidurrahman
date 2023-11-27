d2b59118f05de581a3d82acb953e86b23c03b21c
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-11-27 08:03:12 +0600

    added: highlight section

---
+++ src/app/(Website)/_sections/About.js
@@ -0,0 +1,61 @@
+// components
+import PrimaryButton from "@/components/shared/buttons/PrimaryButton";
+import Image from "next/image";
+import Highlight from "./Highlight";
+
+const About = () => {
+    const data = [
+        {
+            id: 1,
+            number: 150,
+            text: "Students",
+            text2: "Transformed their lives",
+        },
+        {
+            id: 2,
+            number: 5,
+            text: "Years",
+            text2: "Of Experiences",
+        },
+    ];
+    return (
+        <section className="bg-white">
+            <section className="py-[50px] md:py-[100px] container grid grid-cols-1 md:grid-cols-2 gap-[30px]">
+                <div className="flex-1 max-w-[450px] h-auto relative imgContainer">
+                    <Image
+                        src="https://res.cloudinary.com/dzlrpspps/image/upload/v1700540627/Projects/Saidur/saidur_sfwsaw.jpg"
+                        fill
+                        className="rounded-md hover:scale-105 duration-300"
+                        title="Saidur Rahman"
+                        alt="Saidur Rahman"
+                    />
+                </div>
+                <div className="flex-1">
+                    <h1 className="text-[30px] text-gray-800 font-bold mb-[20px] md:mb-[30px]">
+                        Meet Saidur Rahman,
+                    </h1>
+                    <p className="text-[20px] font-normal text-[#2c2c2c] mb-[40px] md:mb-[80px]">
+                        I help entrepreneurs improve productivity; achieve goals
+                        by keeping them accountable for habits and actions. Are
+                        you struggling to improve your business, productivity,
+                        get more done and to achieve your goals? If you’re
+                        interested in getting more out of your life & business
+                        contact me for the FREE discovery call.
+                    </p>
+                    <div>
+                        <PrimaryButton
+                            title="Learn more about saidur"
+                            href="/our_story"
+                        />
+                    </div>
+                </div>
+            </section>
+            {/* states */}
+            <section className="py-[50px] md:py-[100px] container">
+                <Highlight highlightData={data} />
+            </section>
+        </section>
+    );
+};
+
+export default About;
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import WebWrapper from "@/components/shared/WebWrapper/WebWrapper";
 import PrimaryButton from "@/components/shared/buttons/PrimaryButton";
-import Image from "next/image";
+import About from "./(Website)/_sections/About";
 import Programms from "./(Website)/_sections/Programms";
 import Reviews from "./(Website)/_sections/Reviews";
 
@@ -24,39 +24,7 @@ export default async function Home() {
                     </div>
                 </section>
                 <Programms />
-                <section className="bg-white">
-                    <section className="py-[50px] md:py-[100px] container grid grid-cols-1 md:grid-cols-2 gap-[30px]">
-                        <div className="flex-1 max-w-[450px] h-auto relative imgContainer">
-                            <Image
-                                src="https://res.cloudinary.com/dzlrpspps/image/upload/v1700540627/Projects/Saidur/saidur_sfwsaw.jpg"
-                                fill
-                                className="rounded-md hover:scale-105 duration-300"
-                                title="Saidur Rahman"
-                                alt="Saidur Rahman"
-                            />
-                        </div>
-                        <div className="flex-1">
-                            <h1 className="text-[30px] text-gray-800 font-bold mb-[20px] md:mb-[30px]">
-                                Meet Saidur Rahman,
-                            </h1>
-                            <p className="text-[20px] font-normal text-[#2c2c2c] mb-[40px] md:mb-[80px]">
-                                I help entrepreneurs improve productivity;
-                                achieve goals by keeping them accountable for
-                                habits and actions. Are you struggling to
-                                improve your business, productivity, get more
-                                done and to achieve your goals? If you’re
-                                interested in getting more out of your life &
-                                business contact me for the FREE discovery call.
-                            </p>
-                            <div>
-                                <PrimaryButton
-                                    title="Learn more about saidur"
-                                    href="/our_story"
-                                />
-                            </div>
-                        </div>
-                    </section>
-                </section>
+                <About />
                 <section className="py-[50px] md:py-[100px] container">
                     <h1 className="text-center text-[40px] md:text-[60px] font-semibold dark:text-black/80">
                         Here's What People <br /> Are Saying
