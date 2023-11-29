29f86de02e48330e7c6c86d793d06c8809c65992
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-11-29 19:36:40 +0600

    sdf

---
+++ src/app/(dashboard)/auth/_components/login.js
@@ -0,0 +1,106 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import * as z from "zod";
+
+// from schema
+const formSchema = z.object({
+    email: z.string().email({
+        message: "Please enter a valid email address",
+    }),
+    password: z.string().min(6, {
+        message: "password must be at least 6 characters",
+    }),
+});
+const LoginForm = () => {
+    const router = useRouter();
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    // JS Variables
+    const { isSubmitting, isValid } = form.formState;
+
+    const onSubmit = async (values) => {
+        try {
+            const baseURL = process.env.baseURL;
+            const res = await fetch(`${baseURL}/api/auth/login`, {
+                method: "POST",
+                body: JSON.stringify(values),
+            });
+            toast.success("Authentication successful");
+            router.push("/admin");
+        } catch (error) {
+            console.log("login submit error", error);
+        }
+    };
+
+    return (
+        <section className="w-full flex flex-col items-center">
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-8 mt-8 w-full sm:w-[350px]"
+                >
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input
+                                        placeholder="your email"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input
+                                        type="password"
+                                        placeholder="your password"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <div>
+                        <Button
+                            type="submit"
+                            className="w-full"
+                            disabled={!isValid || isSubmitting}
+                        >
+                            Login
+                        </Button>
+                    </div>
+                </form>
+            </Form>
+        </section>
+    );
+};
+
+export default LoginForm;
--- src/app/(dashboard)/auth/page.js
+++ src/app/(dashboard)/auth/page.js
@@ -1,119 +1,25 @@
-"use client";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-// import { zodResolver } from "@hookform/resolvers/zod"
-// import { useForm } from "react-hook-form"
-import { Button } from "@/components/ui/button";
-import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { useRouter } from "next/navigation";
-import { toast } from "react-toastify";
-import * as z from "zod";
+import LoginForm from "./_components/login";
 
-const formSchema = z.object({
-    email: z.string().email({
-        message: "Please enter a valid email address",
-    }),
-    password: z.string().min(6, {
-        message: "password must be at least 6 characters",
-    }),
-});
-
-const AdminLogin = () => {
-    const form = useForm({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            email: "",
-            password: "",
-        },
-    });
-
-    const router = useRouter();
-
-    const { isSubmitting, isValid } = form.formState;
-
-    async function submit(values) {
-        try {
-            const baseURL = process.env.baseURL;
-            await fetch(`${baseURL}/api/auth/login`, {
-                method: "POST",
-                body: JSON.stringify(values),
-            });
-            toast.success("Authentication successful");
-            router.push("/admin");
-        } catch (error) {
-            console.log("login submit error", error);
-        }
-    }
+const Login = () => {
     return (
-        <section className="min-h-screen w-full flex items-center justify-center">
-            <div className="w-full md:max-w-[1000px] flex">
-                <div className="flex-1 bg-black/80"></div>
-                <div className="mx-auto flex-1 w-full flex-col justify-center space-y-6 sm:w-[350px] border p-32">
-                    <div className="flex flex-col space-y-2 text-center">
-                        <h1 className="text-2xl font-semibold tracking-tight">
+        <section className="min-h-screen w-full flex justify-center items-center">
+            <section className="border w-full md:max-w-[1200px] h-auto flex">
+                <section className="hidden md:block w-[30%] bg-black/80"></section>
+                <section className="flex-1 flex-col justify-center items-center p-32  space-y-6">
+                    <div className="space-y-2">
+                        <h1 className="text-2xl font-semibold tracking-tight text-center">
                             Login Now
                         </h1>
-                        <p className="text-sm text-muted-foreground">
+                        <p className="text-sm text-muted-foreground text-center">
                             Enter your email and password below to access admin
                             panel
                         </p>
                     </div>
-                    <Form {...form}>
-                        <form
-                            onSubmit={form.handleSubmit(submit)}
-                            className="space-y-6"
-                        >
-                            <FormField
-                                control={form.control}
-                                name="email"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormControl>
-                                            <Input
-                                                placeholder="your email"
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="password"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormControl>
-                                            <Input
-                                                type="password"
-                                                placeholder="your password"
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <Button
-                                type="submit"
-                                className="w-full"
-                                disabled={!isValid || isSubmitting}
-                            >
-                                Login
-                            </Button>
-                        </form>
-                    </Form>
-                </div>
-            </div>
+                    <LoginForm />
+                </section>
+            </section>
         </section>
     );
 };
 
-export default AdminLogin;
+export default Login;
