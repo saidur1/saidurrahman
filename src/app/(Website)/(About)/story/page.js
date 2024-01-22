8feb270142d9ac94d61b9d2953e2dba97b8a7d59
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-01-22 18:29:20 +0600

    hide companies slider

--- src/app/(Website)/(About)/story/page.js
+++ src/app/(Website)/(About)/story/page.js
@@ -1,11 +1,23 @@
 import WebWrapper from "@/components/shared/WebWrapper/WebWrapper";
 
 const Our_Story = () => {
-    return (
-        <WebWrapper>
-            <div>Our_Story</div>
-        </WebWrapper>
-    );
+  return (
+    <WebWrapper>
+      <section className="w-full  min-h-[calc(100vh-145px)]  flex">
+        <div className="relative w-full md:w-1/3 mx-auto h-[400px] flex justify-center flex-col items-center">
+          <h1 className="text-[60px]">Our Story</h1>
+          <p className="text-sm text-center">
+            Empower your journey with Saidur Rahman, your trusted life and
+            productivity coach. Dive into transformative stories, gain
+            insightful tips, and adopt proven strategies for personal growth and
+            enhanced productivity. Let Saidur guide you towards unlocking your
+            full potential, setting meaningful goals, and cultivating a
+            purposeful life. Join us on the path to success!
+          </p>
+        </div>
+      </section>
+    </WebWrapper>
+  );
 };
 
 export default Our_Story;
--- src/app/page.js
+++ src/app/page.js
@@ -1,41 +1,40 @@
 import WebWrapper from "@/components/shared/WebWrapper/WebWrapper";
 import PrimaryButton from "@/components/shared/buttons/PrimaryButton";
 import About from "./(Website)/_sections/About";
-import CompaniesSlider from "./(Website)/_sections/CompaniesSlider";
 import Programms from "./(Website)/_sections/Programms";
 import Reviews from "./(Website)/_sections/Reviews";
 
 export default async function Home() {
-    return (
-        <WebWrapper>
-            <main className="bg-[#DFE3E4]">
-                <section className="bg-gray-100 h-auto py-[50px] md:min-h-[45vh]">
-                    <div className="container flex flex-col gap-[30px] justify-center items-center h-full">
-                        <h1 className="text-black/80 text-[50px] font-bold text-center">
-                            Accountability coaching for <br /> entrepreneurs.
-                        </h1>
-                        <p className="text-black/80 text-[17px] font-medium text-center">
-                            We help ENTREPRENEURS stay ACCOUNTABLE so they
-                            achieve their financial goals
-                        </p>
-                        <PrimaryButton
-                            href="/schedule"
-                            title="Book A FREE Discovery Call"
-                        />
-                    </div>
-                </section>
-                <Programms />
-                <About />
-                <section className="py-[50px] md:py-[100px] container">
-                    <h1 className="text-center text-[40px] md:text-[60px] font-semibold dark:text-black/80">
-                        Here's What People <br /> Are Saying
-                    </h1>
-                    <div>
-                        <Reviews />
-                    </div>
-                </section>
-                <CompaniesSlider />
-            </main>
-        </WebWrapper>
-    );
+  return (
+    <WebWrapper>
+      <main className="bg-[#DFE3E4]">
+        <section className="bg-gray-100 h-auto py-[50px] md:min-h-[45vh]">
+          <div className="container flex flex-col gap-[30px] justify-center items-center h-full">
+            <h1 className="text-black/80 text-[50px] font-bold text-center">
+              Accountability coaching for <br /> entrepreneurs.
+            </h1>
+            <p className="text-black/80 text-[17px] font-medium text-center">
+              We help ENTREPRENEURS stay ACCOUNTABLE so they achieve their
+              financial goals
+            </p>
+            <PrimaryButton
+              href="/schedule"
+              title="Book A FREE Discovery Call"
+            />
+          </div>
+        </section>
+        <Programms />
+        <About />
+        <section className="py-[50px] md:py-[100px] container">
+          <h1 className="text-center text-[40px] md:text-[60px] font-semibold dark:text-black/80">
+            Here's What People <br /> Are Saying
+          </h1>
+          <div>
+            <Reviews />
+          </div>
+        </section>
+        {/* <CompaniesSlider /> */}
+      </main>
+    </WebWrapper>
+  );
 }
