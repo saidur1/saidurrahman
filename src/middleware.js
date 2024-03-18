a0567643a05eae89cfa1ce1688d430b46c3abf59
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-03-18 16:45:52 +0600

    added: bigin redirect

--- src/middleware.js
+++ src/middleware.js
@@ -2,75 +2,80 @@ import { jwtVerify } from "jose";
 import { NextResponse } from "next/server";
 
 const tokenVerify = async (token) => {
-    // if token is expired
-    try {
-        const key = new TextEncoder().encode(process.env.JWT_SECRET);
+  // if token is expired
+  try {
+    const key = new TextEncoder().encode(process.env.JWT_SECRET);
 
-        // if token is invalid
-        // if token is not valid it will go the the catch block
-        const decodedString = await jwtVerify(token, key);
+    // if token is invalid
+    // if token is not valid it will go the the catch block
+    const decodedString = await jwtVerify(token, key);
 
-        const payload = decodedString?.payload;
+    const payload = decodedString?.payload;
 
-        // Get the current timestamp
-        const currentTimestamp = Math.floor(Date.now() / 1000);
+    // Get the current timestamp
+    const currentTimestamp = Math.floor(Date.now() / 1000);
 
-        // Check if the token has expired
-        if (payload.exp && currentTimestamp >= payload.exp) {
-            // Token has expired
-            console.log("token has expired");
+    // Check if the token has expired
+    if (payload.exp && currentTimestamp >= payload.exp) {
+      // Token has expired
+      console.log("token has expired");
 
-            return false;
-        } else {
-            // Token is still valid
-            return true;
-        }
-    } catch (error) {
-        console.log("[token verification errror]", error);
-        // cookies().delete("authToken");
-
-        return false;
+      return false;
+    } else {
+      // Token is still valid
+      return true;
     }
+  } catch (error) {
+    console.log("[token verification errror]", error);
+    // cookies().delete("authToken");
+
+    return false;
+  }
 };
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request) {
-    const path = request.nextUrl.pathname;
-    const isAccess = request.cookies.get("prospect");
-    const adminToken = request.cookies.get("authToken");
+  const path = request.nextUrl.pathname;
+  const isAccess = request.cookies.get("prospect");
+  const adminToken = request.cookies.get("authToken");
 
-    // handle admin access
-    if (path.startsWith("/admin")) {
-        if (!adminToken) {
-            return NextResponse.redirect(new URL("/auth", request.url));
-        }
-        const authenticated = await tokenVerify(adminToken["value"]);
-        if (!authenticated) {
-            return NextResponse.redirect(new URL("/auth", request.url));
-        }
-        return NextResponse.next();
+  // handle admin access
+  if (path.startsWith("/admin")) {
+    if (!adminToken) {
+      return NextResponse.redirect(new URL("/auth", request.url));
     }
+    const authenticated = await tokenVerify(adminToken["value"]);
+    if (!authenticated) {
+      return NextResponse.redirect(new URL("/auth", request.url));
+    }
+    return NextResponse.next();
+  }
 
-    if (path === "/free-training/content" && !isAccess) {
-        return NextResponse.redirect(new URL("/free-training", request.url));
-    } else if (path === "/free-training" && isAccess) {
-        return NextResponse.redirect(
-            new URL("/free-training/content", request.url)
-        );
-    } else if (path == "/community") {
-        return NextResponse.redirect("https://discord.gg/uxfQY7HmQS");
-    } else {
-        return NextResponse.next();
-    }
+  if (path === "/free-training/content" && !isAccess) {
+    return NextResponse.redirect(new URL("/free-training", request.url));
+  } else if (path === "/free-training" && isAccess) {
+    return NextResponse.redirect(
+      new URL("/free-training/content", request.url)
+    );
+  } else if (path == "/community") {
+    return NextResponse.redirect("https://discord.gg/uxfQY7HmQS");
+  } else if (path === "/l") {
+    return NextResponse.redirect(
+      "https://us.bigin.online/org836263977/forms/f"
+    );
+  } else {
+    return NextResponse.next();
+  }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: [
-        "/",
-        "/free-training",
-        "/free-training/content",
-        "/community",
-        "/admin/:path*",
-    ],
+  matcher: [
+    "/",
+    "/free-training",
+    "/free-training/content",
+    "/community",
+    "/l",
+    "/admin/:path*",
+  ],
 };
