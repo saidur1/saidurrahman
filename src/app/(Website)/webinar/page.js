07a2469e1f1440e14cdc980209e5f4cf11b954e1
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-03-23 15:39:45 +0600

    addedL webinar route

---
+++ src/app/(Website)/webinar/page.js
@@ -0,0 +1,16 @@
+const Page = () => {
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center">
+      <iframe
+        width="360"
+        height="240"
+        src="https://webinar.zoho.com/meeting/register/embed?uId=4222902000000031001"
+        frameborder="0"
+      >
+        {" "}
+      </iframe>
+    </div>
+  );
+};
+
+export default Page;
