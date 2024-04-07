31c6e03314360ef719a71d4e0b2b391539403077
saidur1 <saidur.rahman@live.com>
2024-04-07 15:22:01 +0600

    Update page.js

--- src/app/(Website)/work-with-me/page.js
+++ src/app/(Website)/work-with-me/page.js
@@ -246,7 +246,7 @@ const Page = () => {
                   "text-[#2D2D2D] text-[15px] w-full md:w-[456px] mx-auto text-center"
                 )}
               >
-                All she was missing was accountability, She increased productivity in a day to a level that she didn't realise was possible.
+                All she was missing was accountability, She increased productivity to a level that she didn't realise was possible.
               </p>
               <p
                 className={cn(
