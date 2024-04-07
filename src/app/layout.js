70bda8a44ff002b155e1419b73a41b725daae69b
saidur1 <saidur.rahman@live.com>
2024-04-07 16:25:20 +0600

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,6 +27,19 @@ export default function RootLayout({ children }) {
         {/* Google analytics */}
         <ToastContainer />
         <CookiesProvider>{children}</CookiesProvider>
+  <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5d7b72919f6b7a4457e180c6/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
       </body>
     </html>
   );
