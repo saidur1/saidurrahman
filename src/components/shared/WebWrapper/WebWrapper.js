707b1d67e4094d138633542dee21ee772b3c6ebb
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-04-08 08:28:41 +0600

    nad

--- src/components/shared/WebWrapper/WebWrapper.js
+++ src/components/shared/WebWrapper/WebWrapper.js
@@ -3,6 +3,7 @@ import Navbar from "@/components/navbar/navbar";
 const WebWrapper = ({ children }) => {
   return (
     <>
+      {/* this is navbar */}
       <Navbar />
       {children}
     </>
