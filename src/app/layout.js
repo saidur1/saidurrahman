1b1c287dc4f8308fe1a744cb228ebacfc7ad223d
saidur1 <saidur.rahman@live.com>
2025-03-19 17:24:27 +0600

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
         {/* Live Chat */}
         {/* <LiveChat /> */}
         {/* Google analytics */}
-        <GoogleAnalytics />
+
         {/* Google analytics */}
         <ToastContainer />
         <CookiesProvider>{children}</CookiesProvider>
